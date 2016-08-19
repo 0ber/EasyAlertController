f8b174c64897e8bd587e4f7941c39c64374fed8b
Ober01 <kalinkin.aleksey@gmail.com>
2016-08-19 22:18:30 +0300

    added Carthage support

---
+++ EasyAlertController/EasyAlertController.h
@@ -0,0 +1,19 @@
+//
+//  EasyAlertController.h
+//  EasyAlertController
+//
+//  Created by Alex on 19/08/16.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyAlertController.
+FOUNDATION_EXPORT double EasyAlertControllerVersionNumber;
+
+//! Project version string for EasyAlertController.
+FOUNDATION_EXPORT const unsigned char EasyAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyAlertController/PublicHeader.h>
+
+
